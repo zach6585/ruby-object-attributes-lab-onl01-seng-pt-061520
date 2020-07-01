687ea9388e162d1a033df13afe7dd51c7bf92632
zachary mines <zmines@wesleyan.edu>
2020-07-01 02:05:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(memes)
+    @name = memes 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def breed=(type)
+    @breed = type 
+  end 
+  
+  def breed 
+    @breed 
+  end 
+end 
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person 
+  def name=(nem)
+    @name = nem 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def job=(please_clap)
+    @job = please_clap
+  end 
+  
+  def job 
+    @job 
+  end 
+end 
